bb2c0845706ec92c609422ec4c68aeda2d905a8b
Łukasz Król <lionel@o2.pl>
2019-01-26 20:29:12 +0100

    Correct indentations

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ server.on("request", function(request, response) {
       if (err) throw err;
       response.write(data);
       response.end();
-    })
-    } else {
-        response.statusCode = 404;
-        response.write('<img src="https://img.freepik.com/darmowe-wektory/404-projekt-b%C5%82%C4%99du-z-robotami-drogowymi_23-2147735305.jpg?size=338&ext=jpg" alt="404">');
-        response.end();
-      }
-  });
+    });
+  } else {
+    response.statusCode = 404;
+    response.write('<img src="https://img.freepik.com/darmowe-wektory/404-projekt-b%C5%82%C4%99du-z-robotami-drogowymi_23-2147735305.jpg?size=338&ext=jpg" alt="404">');
+    response.end();
+  }
+});
 
 server.listen(8080);
